5d9a027dbeb3ef6ce46504f1f37f6fabcdec03eb
lifeflavor0425 <rjsdnakfmzh61@gmail.com>
2021-02-24 07:51:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask , render_template, request, redirect
+from scrapper import get_jobs
+app = Flask("SuperScappper")
+db={}
+@app.route("/")
+def home():
+    return render_template("potato.html")
+
+@app.route("/report")
+def report():
+  word= request.args.get('word')
+  if word:
+    word=word.lower()
+    fromDb = db.get(word)
+    if fromDb:
+      jobs=fromDb
+    else :
+      jobs=get_jobs(word)
+      db[word]=jobs
+  else :
+    return redirect("/")
+  return render_template("report.html",searchingBy=word, resultNumber=len(jobs))
+
+app.run(host="0.0.0.0")
