14ddd16ef3afaff943a1c019ac2dfc47567c880e
lifeflavor0425 <rjsdnakfmzh61@gmail.com>
2021-02-25 05:19:47 +0000

    노마드 코더 파이썬 끝

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask , render_template, request, redirect
+from flask import Flask , render_template, request, redirect, send_file
 from scrapper import get_jobs
+from exporter import save_to_file
+
 app = Flask("SuperScappper")
 db={}
+
 @app.route("/")
 def home():
     return render_template("potato.html")
@@ -11,14 +14,33 @@ def report():
   word= request.args.get('word')
   if word:
     word=word.lower()
-    fromDb = db.get(word)
-    if fromDb:
-      jobs=fromDb
+    existingjobs = db.get(word)
+    if existingjobs:
+      jobs=existingjobs
     else :
       jobs=get_jobs(word)
       db[word]=jobs
   else :
     return redirect("/")
-  return render_template("report.html",searchingBy=word, resultNumber=len(jobs))
-
+  return render_template(
+    "report.html",
+    searchingBy=word, 
+    resultNumber=len(jobs),
+    jobs=jobs)
+  
+@app.route("/export")
+def export():
+  try:
+    word= request.args.get('word')
+    if not word:
+      raise Exception();
+    word=word.lower()
+    jobs=db.get(word)
+    if not jobs:
+      raise Exception();
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+  except:
+    redirect("/")
+ 
 app.run(host="0.0.0.0")
